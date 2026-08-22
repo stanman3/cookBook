58e4cb279ef47f611a54fb04d011889f4db2c5ad
stanman3 <stanislav.manolov.1@gmail.com>
2026-08-22 13:27:26 +0300

    db

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,11 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
+load_dotenv()
 
-URL_DATABASE = 'postgresql://postgres:Rctvybun97.@localhost:5432/CookBook'
+URL_DATABASE = os.getenv("DATABASE_URL", "postgresql://postgres:Rctvybun97.@localhost:5432/CookBook")
 
 engine = create_engine(URL_DATABASE)
 
