8d6f71a703390ee9595df5eadfec2efdea182910
stanman3 <stanislav.manolov.1@gmail.com>
2026-08-22 14:39:19 +0300

    test1

--- backend/database.py
+++ backend/database.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:Rctvybun97.@localhost:5432/CookBook")
 
-print("DATABASE_URL:", os.getenv("DATABASE_URL"))
+print("DATABASE_URL exists:", DATABASE_URL is not None)
 
 engine = create_engine(DATABASE_URL)
 
